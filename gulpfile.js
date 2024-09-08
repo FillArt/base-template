12d4e70de9761da47fb1939378686d8a5368c9a1
FillArt <artiom.filipopovschii@gmail.com>
2024-09-07 22:46:22 -0300

    Wrote a task for Gulp to optimize SVG and created a sprite for more literate use in the project

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,38 @@
 const { src, dest, parallel, series, watch } = require('gulp');
 
 // Gulp plugins
-const concat    = require('gulp-concat');
-const cleanCSS  = require('gulp-clean-css'); // Minified CSS plugin
-const rename    = require('gulp-rename');
+const concat    = require('gulp-concat');       // Merge files into one
+const cleanCSS  = require('gulp-clean-css');    // Minified CSS plugin
+const rename    = require('gulp-rename');       // Rename plugin
+const svgmin    = require('gulp-svgmin')        // Minified SVG plugin
+const svgSprite = require('gulp-svg-sprite');   // Create SVG sprite
+
+// SVG Sprite configuration
+const spriteConfig = {
+    mode: {
+        symbol: {
+            sprite: 'sprite.svg',   // Path to SVG sprite
+            example: false          // No generated html example
+        }
+    }
+};
+
+// Function for optimization and creation of sprite
+const createSprite = () => {
+    return src('app/assets/svg/*.svg')  // Path to SVG files
+        .pipe(svgmin({                  // Use svgmin for optimization
+            plugins: [
+                { removeViewBox: false },
+                { cleanupIDs: false }
+            ]
+        }))
+        .pipe(svgSprite(spriteConfig))  // Create SVG sprite
+        .pipe(dest('dist/svg')); // Путь к выходной директории
+}
+
 
 // Function to minify CSS files
-let minifyCSS = () => {
+const minifyCSS = () => {
     return src('app/assets/css/*.css')      // Path to CSS source files
         .pipe(concat('styles.css'))         // Concatenates all CSS files into one file
         .pipe(cleanCSS())                   // Minified CSS
@@ -23,3 +49,4 @@ function watchFiles() {
 }
 
 exports.default = testTask;
+exports.createSprite = createSprite;
